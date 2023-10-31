b4755bf6e35e35944937983521b3210517650f74
ritaOmondi <cherylritaomondi@gmail.com>
2023-10-31 05:08:45 -0700

    linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,25 +1,55 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-* insert_node - inserts a number into a sorted singly linked list
-* @head: pointer to head node
-* @number: value to be inserted
-*
-* Return: address of new node or NULL on failure
-*/
+ * insert_node - Inserts a number into a sorted singly linked list.
+ *
+ * @head: Double pointer to a singly linked list
+ *
+ * @number: Value of the new node.
+ *
+ * Return: The address of the new node, or NULL if it failed.
+ */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-		listint_t *current = &head;
-		listint_t *next = current->next;
-		while (current->next != '\0')
-		{
-				if (next->n > number)
-				{
-						current->n = number;
-						current->next = current->next;
-						return (current);
-				}
-				current = current->next;
-		}
+	int flag = 0;
+	listint_t *new_node = NULL, *actual = NULL, *after = NULL;
+
+	if (head == NULL)
 		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number, new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	actual = *head;
+	if (number <= actual->n)
+	{
+		new_node->next = actual, *head = new_node;
+		return (*head);
+	}
+	if (number > actual->n && !actual->next)
+	{
+		actual->next = new_node;
+		return (new_node);
+	}
+	after = actual->next;
+	while (actual)
+	{
+		if (!after)
+			actual->next = new_node, flag = 1;
+		else if (after->n == number)
+			actual->next = new_node, new_node->next = after, flag = 1;
+		else if (after->n > number && actual->n < number)
+			actual->next = new_node, new_node->next = after, flag = 1;
+		if (flag)
+			break;
+		after = after->next, actual = actual->next;
+	}
+	return (new_node);
 }
