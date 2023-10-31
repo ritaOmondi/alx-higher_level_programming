bd4d1714945dcaafc26557530ef5b5d0d06342a1
ritaOmondi <cherylritaomondi@gmail.com>
2023-10-31 05:46:18 -0700

    lower and upper cases

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 output = ""
 for i in range(ord('z'), ord('a') - 1, -1):
     letter = chr(i)
-    case = letter.lower() if i % 2 == 0 else letter.upper()
+    case = letter.islower() if i % 2 == 0 else letter.isupper()
     output += case
 
 print(output, end="")
