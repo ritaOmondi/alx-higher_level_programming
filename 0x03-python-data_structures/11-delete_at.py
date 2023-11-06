f6fb860bc289c4cf4adcc4d8d765323d98fadf5e
ritaOmondi <cherylritaomondi@gmail.com>
2023-11-06 01:12:51 -0800

    deleting an item in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+     new_list = []
+     for i, item in enumerate(my_list):
+         if i != idx:
+             new_list.append(item)
+    return new_list
