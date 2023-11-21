56aa4ac14707c3be233cb8163159e94a3ffa36bb
ritaOmondi <cherylritaomondi@gmail.com>
2023-11-21 01:37:40 -0800

    introducing the size of the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """This is the class square"""
+    def __init(self, size):
+        """create a constructor
+        Args:
+        size (intgeer) size of the square"""
+        self.__size = size
