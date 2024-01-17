d0df49d3402655caf1412242bfef8d349026c72a
ritaomondi <cherylritaomondi@gmail.com>
2024-01-17 02:04:58 -0800

    create table with NOT  NULL values

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- creating a table and objects if not exist
+-- creates the table force_name
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL,
+	id INT,
+	name VARCHAR(256) NOT NULL
 );
