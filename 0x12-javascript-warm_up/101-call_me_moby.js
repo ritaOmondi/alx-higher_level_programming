ea7c53748c298aa2915948f78162f46be76f6562
ritaOmondi <cherylritaomondi@gmail.com>
2024-02-12 02:13:36 -0800

    multplying x by the function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
