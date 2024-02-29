cddbfff7f20ed82d77aecca3fbf810710f2390fc
ritaOmondi <cherylritaomondi@gmail.com>
2024-02-29 03:07:17 -0800

    Post JSON

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL and displays the body of the response
-URL="$1"
-JSON_FILE="$2"
-
-# Check if the JSON file is valid
-if ! python3 -m json.tool "$JSON_FILE" > /dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-# Send POST request and display response body
-curl -s -X POST -H "Content-Type: application/json" -d @"$JSON_FILE" "$URL"
-
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
