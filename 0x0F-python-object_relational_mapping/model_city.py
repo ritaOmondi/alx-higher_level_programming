be2b4a58d9a24e306cb88cd882b77ea3d0371ebd
ritaOmondi <cherylritaomondi@gmail.com>
2024-04-10 23:41:46 -0700

    city table

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-
+"""Start link class to table in database 
 """
-Write a python file that contains the class definition of a State
-and an instance Base.
-"""
+import sys
+from model_state import Base, State
 
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-
-class State(Base):
-
-    """
-    Class with attributes of a state.
-    """
-
-    __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
