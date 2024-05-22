50f476f77fb800d05475b0bc2cd485d745482ff3
ritaOmondi <cherylritaomondi@gmail.com>
2024-05-22 02:53:40 -0700

    appending string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-function writeToFile(filePath, content) {
-  try {
-    fs.writeFileSync(filePath, content, { encoding: 'utf-8' });
-    console.log('File written successfully!');
-  } catch (error) {
-    console.error('Error occurred while writing to file:', error);
-  }
-}
-
-// Usage example
-//const filePath = process.argv[2]; // First command-line argument
-//const stringToWrite = process.argv[3]; // Second command-line argument
-
-//writeToFile(filePath, stringToWrite);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
